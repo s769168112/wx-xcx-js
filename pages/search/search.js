a2e208c44107ba7478dc8bf612accb92f5a8c7ad
s769168112 <769168112@qq.com>
2021-04-26 23:03:36 +0800

    update

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,54 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    searchRes: [
-      {
-        id: 1,
-        label: '肝囊肿',
-        opacity:1,
-      },
-      {
-        id: 2,
-        label: '脂肪肝',
-        opacity:1,
-      },
-      {
-        id: 3,
-        label: '消化不良是肝的问题？',
-        opacity:1,
-      },
-      {
-        id: 4,
-        label: '肝硬化',
-        opacity:1,
-      },
-      {
-        id: 5,
-        label: '酒精肝',
-        opacity:1,
-      },
-    ],
-    res: [],
-    searchTransition: false,
+    searchRes: [],
     searchView: 0,
     boxHeight: 0,
-
+    searchText:'',
     page:1,
   },
-
+  blurSearch:function(e){
+    this.setData({
+      searchText:e.detail.value
+    })
+  },
   searchChange:function(e){
-    const value = e.detail.value
+    
     const api = require("../../api/other/other.service").OtherHttpService.prototype
-    // api.getSurgeryListData().then(res => {
-    //   console.log('获取手术全解',res)
-    //   this.setData({
-    //     surgeryList:res.dataList
-    //   })
-    // })
-    console.log(e)
     let params = {
       pageIndex:this.data.page,
-      search:value
+      pageSize:20,
+      search:value,
     }
     api.getSearchData(params).then(res => {
       console.log('搜索结果',res)
@@ -60,21 +30,26 @@ Page({
   },
 
   search: function () {
-    this.data.searchView++
-    if(this.data.searchView <= this.data.searchRes.length) {
-      this.setData({
-        res: [...this.data.searchRes].splice(0, this.data.searchView),
-        boxHeight: (this.data.res.length + 1) * 60
-      })
+    const api = require("../../api/other/other.service").OtherHttpService.prototype
+    let params = {
+      pageIndex:this.data.page,
+      pageSize:20,
+      search:this.data.searchText
     }
-  },
-  del:function () {
-    this.data.searchView --
-    this.setData({
-      res: [...this.data.searchRes].splice(0, this.data.searchView),
-      boxHeight: (this.data.res.length - 1) * 60
+    api.getSearchData(params).then(res => {
+      console.log('搜索结果',res)
+      this.setData({
+        searchRes:res.dataList
+      })
     })
   },
+  // del:function () {
+  //   this.data.searchView --
+  //   this.setData({
+  //     res: [...this.data.searchRes].splice(0, this.data.searchView),
+  //     boxHeight: (this.data.res.length - 1) * 60
+  //   })
+  // },
 
   /**
    * 生命周期函数--监听页面加载
